2d7dd7aac93deeffe44aa3c45d7c67b58119af88
melissawelty <65548626+melissawelty@users.noreply.github.com>
2020-06-23 06:54:43 -0500

    configure search url

--- food_rec.py
+++ food_rec.py
@@ -16,13 +16,35 @@ client = Client(OPEN_YELP_API_KEY)
 MY_ZIP = os.getenv("MY_ZIP", default="73103") 
 #COUNTRY_CODE = os.getenv("COUNTRY_CODE", default="US")
 
+#id = input("Please enter city name or zip: ")
+
+request_url = "https://api.yelp.com/v3/businesses/search"
+HEADERS = {'Authorization': 'bearer %s' % OPEN_YELP_API_KEY} 
+
+
+
+# business_response = client.business.get_by_id('yelp-san-francisco')
+
+# business_response
+# Business(alias='yelp-san-francisco', attributes=None, categories=[Category(alias='massmedia', title='Mass Media')], coordinates=Coordinates(latitude=37.7867703362929, longitude=-122.399958372115), display_phone='(415) 908-3801', hours=[Hours(hours_type='REGULAR', is_open_now=True, open=[DayHours(day=0, end='1800', is_overnight=False, start='0800'), DayHours(day=1, end='1800', is_overnight=False, start='0800'), DayHours(day=2, end='1800', is_overnight=False, start='0800'), DayHours(day=3, end='1800', is_overnight=False, start='0800'), DayHours(day=4, end='1800', is_overnight=False, start='0800')])], id='4kMBvIEWPxWkWKFN__8SxQ', image_url='https://s3-media2.fl.yelpcdn.com/bphoto/nQK-6_vZMt5n88zsAS94ew/o.jpg', is_claimed=True, is_closed=False, location=Location(address1='140 New Montgomery St', address2='', address3='', city='San Francisco', country='US', cross_streets='Natoma St & Minna St', display_address=['140 New Montgomery St', 'San Francisco, CA 94105'], state='CA', zip_code='94105'), name='Yelp', phone='+14159083801', photos=['https://s3-media2.fl.yelpcdn.com/bphoto/nQK-6_vZMt5n88zsAS94ew/o.jpg', 'https://s3-media2.fl.yelpcdn.com/bphoto/yFHIb9gob4TzhKUemMOPww/o.jpg', 'https://s3-media1.fl.yelpcdn.com/bphoto/EHCfkEpZraIfPl8gvCo1tg/o.jpg'], rating=2.0, review_count=8421, transactions=[], url='https://www.yelp.com/biz/yelp-san-francisco?adjust_creative=wpr6gw4FnptTrk1CeT8POg&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_lookup&utm_source=wpr6gw4FnptTrk1CeT8POg')
+parameters= {'term':'coffee',
+            'limit':50,
+            'radius':10000,
+            'location': 'San Diego'}
+
+response = requests.get(request_url, params=parameters, headers=HEADERS)
+parsed_response = json.loads(response.text)
+
+print(parsed_response)
+# OUTPUT
+
+print("-------------------------")
+print("NAME: {business_name}") 
+print("-------------------------")
+print("TOP 5 BUSINESS NAMES: ")
 
 
 # def get_food_rec(zip_code=MY_ZIP, country_code=COUNTRY_CODE):
-#     # see: https://openweathermap.org/current
-#     request_url = f"https://api.openweathermap.org/data/2.5/forecast?zip={zip_code},{country_code}&units=imperial&appid={OPEN_WEATHER_API_KEY}"
-#     response = requests.get(request_url)
-#     parsed_response = json.loads(response.text)
 #     #print(parsed_response.keys()) #> dict_keys(['cod', 'message', 'cnt', 'list', 'city'])
 #     result = {
 #         "city_name": parsed_response["city"]["name"],
