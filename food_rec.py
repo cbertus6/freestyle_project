e1cb5c12a9639b3922f39978160bd1b68e1a43ba
melissawelty <65548626+melissawelty@users.noreply.github.com>
2020-06-23 19:58:18 -0500

    coordinates into list

--- food_rec.py
+++ food_rec.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 
 
 from yelp.client import Client
+import plotly.graph_objects as go
 
 
 load_dotenv()
@@ -24,7 +25,7 @@ print("THANKS FOR USING YELP BUSINESS SEARCH")
 print("TO BEGIN, PLEASE ENTER THE FOLLOWING CRITERIA TO HELP WITH YOUR SEARCH:")
 
 location = input("Location: ")
-search_term = input("Search term: ")
+search_term = input("Food type: ")
 price_range = input("Price range 1($) - 4($$$$): ")
 limit_results = input("Number of business results to return: ")
 
@@ -36,16 +37,64 @@ parameters= {'term':search_term,
             'radius': 10000,
             'offset': 50,
             'sort_by': 'best_match',
-            'price': price_range}
+            'price': price_range,}
 
 response = requests.get(request_url, params=parameters, headers=HEADERS)
+
+
+
 parsed_response = json.loads(response.text)
+
+# if 'Error' in parsed_response:
+#     print("Invalid search term. Please try again.")
+#     exit()
 
 # OUTPUT
 
+latitude = []
+longitude = []
+name = []
 
 for b in parsed_response['businesses']:
-    print(b['name'])
+    # print(b['name'])
+    latitude.append(b['coordinates']['latitude'])
+    longitude.append(b['coordinates']['longitude'])
+    name.append(b['name'])
+
+
+
+
+mapbox_access_token = open(parsed_response).read()
+
+fig = go.Figure(go.Scattermapbox(
+        lat=['latitude'],
+        lon=['longitude'],
+        mode='markers',
+        marker=go.scattermapbox.Marker(
+            size=9
+        ),
+        text=['name'],
+    ))
+
+fig.update_layout(
+    autosize=True,
+    hovermode='closest',
+    mapbox=dict(
+        accesstoken=mapbox_access_token,
+        bearing=0,
+        center=dict(
+            lat=38.92,
+            lon=-77.07
+        ),
+        pitch=0,
+        zoom=10
+    ),
+)
+
+fig.show()
+
+print("--------------------")
+# we have the coordinates, can it print on a graph with the restaurant name? 
   
 
 
