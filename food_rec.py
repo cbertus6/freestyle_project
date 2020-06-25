76e23d1ff42934fd00a9b1b0d26082764b2edf64
melissawelty <65548626+melissawelty@users.noreply.github.com>
2020-06-25 18:12:01 -0500

    made it work

--- food_rec.py
+++ food_rec.py
@@ -59,8 +59,8 @@ print("--------------------")
 print("Thank you for choosing Yelp! Your recommendations are below:")
 for b in parsed_response['businesses']:
     print(b['name'])
-    latitude.append(b['coordinates']['latitude'])
-    longitude.append(b['coordinates']['longitude'])
+    latitude.append(float(b['coordinates']['latitude']))
+    longitude.append(float(b['coordinates']['longitude']))
     name.append(b['name'])
 print("--------------------")
 print("Enjoy!")
@@ -69,26 +69,55 @@ print("Enjoy!")
 MAPBOX_ACCESS_TOKEN="pk.eyJ1IjoiczJ0MiIsImEiOiJja2J2ZHdxenowNTJwMndxZjdyY3ZhNTV3In0.nqev9fVN8FEibo3cr81YAA"
 
 
+# fig = go.Figure(go.Scattermapbox(
+#         lat=['38.91427','38.91538','38.91458',
+#              '38.92239','38.93222','38.90842',
+#              '38.91931','38.93260','38.91368',
+#              '38.88516','38.921894','38.93206',
+#              '38.91275'],
+#         lon=['-77.02827','-77.02013','-77.03155',
+#              '-77.04227','-77.02854','-77.02419',
+#              '-77.02518','-77.03304','-77.04509',
+#              '-76.99656','-77.042438','-77.02821',
+#              '-77.01239'],
+#         mode='markers',
+#         marker=go.scattermapbox.Marker(
+#             size=9
+#         ),
+#         text=["The coffee bar","Bistro Bohem","Black Cat",
+#              "Snap","Columbia Heights Coffee","Azi's Cafe",
+#              "Blind Dog Cafe","Le Caprice","Filter",
+#              "Peregrine","Tryst","The Coupe",
+#              "Big Bear Cafe"],
+#     ))
+
+# fig.update_layout(
+#     autosize=True,
+#     hovermode='closest',
+#     mapbox=dict(
+#         accesstoken=MAPBOX_ACCESS_TOKEN,
+#         bearing=0,
+#         center=dict(
+#             lat=38.92,
+#             lon=-77.07
+#         ),
+#         pitch=0,
+#         zoom=10
+#     ),
+# )
+
+# fig.show()
+
+
+
 fig = go.Figure(go.Scattermapbox(
-        lat=['38.91427','38.91538','38.91458',
-             '38.92239','38.93222','38.90842',
-             '38.91931','38.93260','38.91368',
-             '38.88516','38.921894','38.93206',
-             '38.91275'],
-        lon=['-77.02827','-77.02013','-77.03155',
-             '-77.04227','-77.02854','-77.02419',
-             '-77.02518','-77.03304','-77.04509',
-             '-76.99656','-77.042438','-77.02821',
-             '-77.01239'],
+        lat=latitude,
+        lon=longitude,
         mode='markers',
         marker=go.scattermapbox.Marker(
             size=9
         ),
-        text=["The coffee bar","Bistro Bohem","Black Cat",
-             "Snap","Columbia Heights Coffee","Azi's Cafe",
-             "Blind Dog Cafe","Le Caprice","Filter",
-             "Peregrine","Tryst","The Coupe",
-             "Big Bear Cafe"],
+        text=name,
     ))
 
 fig.update_layout(
@@ -98,8 +127,8 @@ fig.update_layout(
         accesstoken=MAPBOX_ACCESS_TOKEN,
         bearing=0,
         center=dict(
-            lat=38.92,
-            lon=-77.07
+            lat=parsed_response['region']['center']['latitude'],
+            lon=parsed_response['region']['center']['longitude']
         ),
         pitch=0,
         zoom=10
@@ -107,35 +136,6 @@ fig.update_layout(
 )
 
 fig.show()
-
-# mapbox_access_token = open(parsed_response).read()
-
-# fig = go.Figure(go.Scattermapbox(
-#         lat=['latitude'],
-#         lon=['longitude'],
-#         mode='markers',
-#         marker=go.scattermapbox.Marker(
-#             size=9
-#         ),
-#         text=['name'],
-#     ))
-
-# fig.update_layout(
-#     autosize=True,
-#     hovermode='closest',
-#     mapbox=dict(
-#         accesstoken=mapbox_access_token,
-#         bearing=0,
-#         center=dict(
-#             lat=parsed_response['region']['center']['latitude'],
-#             lon=parsed_response['region']['center']['longitude']
-#         ),
-#         pitch=0,
-#         zoom=10
-#     ),
-# )
-
-# fig.show()
 
 
  
